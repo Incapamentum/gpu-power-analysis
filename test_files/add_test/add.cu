5b1f98f40aff6ab734936cf2f3ee0770720093f1
Incapamentum <diazg.gustavoa@gmail.com>
2022-04-01 17:01:53 -0400

    Cleaned up directory. Done with testing

--- test_files/add_test/add.cu
+++
@@ -1,9 +0,0 @@
-#include "add.cuh"
-
-// Kernel function to add the elements of two arrays
-__global__
-void add(int n, float *x, float *y)
-{
-    for (int i = 0; i < n; i++)
-        y[i] = x[i] + y[i];
-}
--- test_files/add_test/add.cuh
+++
@@ -1,7 +0,0 @@
-#ifndef ADD_CUH
-#define ADD_CUH
-
-__global__
-void add(int n, float *x, float *y);
-
-#endif
