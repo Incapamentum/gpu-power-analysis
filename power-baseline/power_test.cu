121de863be29b6407d417ce3d859ce8f9b147775
Incapamentum <diazg.gustavoa@gmail.com>
2022-05-01 23:31:21 -0400

    Completed power test

--- power-baseline/power_test.cu
+++ power-baseline/power_test.cu
@@ -1,38 +1,104 @@
 #include "../program_benchmarks/definitions.cuh"
+
 #include "../program_benchmarks/mm_mul/mm_mul.cuh"
+#include "../program_benchmarks/mv_mul/mv_mul.cuh"
 
-#include "nvmlPower.hpp"
+#include <stdio.h>
+#include <string.h>
 
-int main(void)
+void matrixMultiplication(float *c, float *a, float *b, int n)
+{
+    float *c_dev, *a_dev, *b_dev;
+    int size;
+
+    size = n * n * sizeof(float);
+
+    cudaMalloc((void **)&a_dev, size);
+    cudaMemcpy(a_dev, a, size, cudaMemcpyHostToDevice);
+    cudaMalloc((void **)&b_dev, size);
+    cudaMemcpy(b_dev, b, size, cudaMemcpyHostToDevice);
+
+    cudaMalloc((void **)&c_dev, size);
+
+    mm_mul<<<1, 1>>>(c_dev, a_dev, b_dev, n);
+
+    cudaFree(c_dev);
+    cudaFree(a_dev);
+    cudaFree(b_dev);    
+}
+
+void matrixVectorMultiply(float *c, float *a, float *b, int n)
+{
+    float *a_dev, *b_dev, *c_dev;
+    int size;
+
+    size = n * sizeof(int);
+
+    cudaMalloc((void **)&a_dev, size * size);
+    cudaMalloc((void **)&b_dev, size);
+    cudaMalloc((void **)&c_dev, size);
+
+    cudaMemcpy(a_dev, a, size * size, cudaMemcpyHostToDevice);
+    cudaMemcpy(b_dev, b, size, cudaMemcpyHostToDevice);
+
+    mv_mul<<<1, 1>>>(a_dev, b_dev, c_dev, n);
+
+    cudaFree(a_dev); cudaFree(b_dev); cudaFree(c_dev);
+}
+
+int main(int argc, char **argv)
 {
     dim3 Db = dim3(1);
     dim3 Dg = dim3(1);
 
-    int n = 10;
+    int i, j, n = 10;
 
     // Host variables
     float *a, *b, *c;
 
-    // Device variables
-    float *d_a, *d_b, *d_c;
+    if (argc != 2)
+    {
+        printf("Insufficient arguments!\n");
+        return -1;
+    }
 
-    // Allocate host memory
-    a = (float *)malloc(n * sizeof(float));
-    b = (float *)malloc(n * sizeof(float));
-    c = (float *)malloc(n * sizeof(float));
+    a = (float *)malloc(n * n * sizeof(float));
 
-    // Allocate device memory
-    cudaMalloc((void **)&d_a, n * sizeof(float));
-    cudaMalloc((void **)&d_b, n * sizeof(float));
-    cudaMalloc((void **)&d_c, n * sizeof(float));
+    // Allocating memory for matrix-matrix
+    if (strcmp("mm_mul", argv[1]) == 0)
+    {
+        b = (float *)malloc(n * n * sizeof(float));
+        c = (float *)malloc(n * n * sizeof(float));
 
-    nvmlAPIRun();
-    mm_mul<<<Db, Dg>>>(d_c, d_a, d_b, n);
-    nvmlAPIEnd();
+        for (i = 0; i < (n * n); i++)
+        {
+            a[i] = 5;
+            b[i] = 5;
+            c[i] = 0;
+        }
+
+        matrixMultiplication(c, a, b, n);
+    }
+    else if (strcmp("mv_mul", argv[1]) == 0)
+    {
+        b = (float *)malloc(n * sizeof(float));
+        c = (float *)malloc(n * sizeof(float));
+
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                a[i * n + j] = i * n + j + 1;
+            }
+
+            b[i] = i + 1;
+        }
+
+        matrixVectorMultiply(c, a, b, n);
+    }
 
     // Freeing allocated memory
     free(a); free(b); free(c);
-    cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
 
     return 0;
 }
